b8cb0ee135a980fcc1f3df32df3e0e30998d8602
TAEJOON <hazukinoharuka@gmail.com>
2026-04-28 10:51:54 +0900

    テストCommit

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+import java.awt.print.Printable;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// メインメソッド
+
+		int i = 10;
+		
+		i++;
+		
+		System.out.println(args[0]);
+		System.out.println(i);
+		
+	}
+
+}
